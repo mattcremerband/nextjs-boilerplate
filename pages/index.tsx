430223a23e7b0e3bf5baafc4f4dd99b18c854ba6
Matthew Cremer <matthew.cremer@deliverytribe.co.za>
2023-02-05 13:53:12 +0200

    build: add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,13 @@ import PrimaryLayout from '../components/layouts/primary/PrimaryLayout';
 import SidebarLayout from '../components/layouts/sidebar/SidebarLayout';
 import BaseTemplate from '../components/templates/base/BaseTemplate';
 import { mockBaseTemplateProps } from '../components/templates/base/BaseTemplate.mocks';
-import styles from '../styles/Home.module.css';
 import { NextPageWithLayout } from './page';
 
 const Home: NextPageWithLayout = () => {
   return (
-    <section className={styles.main}>
-      <h1 className={styles.title}>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+    <section className="bg-gradient-to-r from-cyan-500 to-blue-500">
+      <h1>
+        <a href="https://nextjs.org">Next.js!</a> Boilerplate with tailwindcss
       </h1>
       <BaseTemplate {...mockBaseTemplateProps.base} />
     </section>
