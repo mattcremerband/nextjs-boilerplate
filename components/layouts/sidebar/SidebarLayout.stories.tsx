14560bd595ea6c0d5a60562c354ec9d59894e658
Matthew Cremer <matthew.cremer@deliverytribe.co.za>
2023-02-05 13:43:49 +0200

    build: create initial layouts

---
+++ components/layouts/sidebar/SidebarLayout.stories.tsx
@@ -0,0 +1,19 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import SidebarLayout, { ISidebarLayout } from './SidebarLayout';
+import { mockSidebarLayoutProps } from './SidebarLayout.mocks';
+
+export default {
+  title: 'layout/SidebarLayout',
+  component: SidebarLayout,
+  argTypes: {},
+} as ComponentMeta<typeof SidebarLayout>;
+
+const Template: ComponentStory<typeof SidebarLayout> = (args) => (
+  <SidebarLayout {...args} />
+);
+
+export const Base = Template.bind({});
+
+Base.args = {
+  ...mockSidebarLayoutProps.base,
+} as ISidebarLayout;
